e427fe32983033fa27fc450e2186a4dc83fd9e40
misakii <misakiigmd@gmail.com>
2024-08-05 15:00:59 +0200

    Finished website

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 function toggleMenu() {
-    const menu = document.querySelector(".menu-links")
-    const icon = document.querySelector(".hamburger-icon")
-    menu.classList.toggle("open")
-    icon.classList.toggle("open")
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+
+function copyEmail() {
+  // Copy the text inside the text field
+  navigator.clipboard.writeText("misakiigmd@gmail.com");
+
+  // Alert the copied text
+  alert("Copied the email to your clipboard!");
 }
